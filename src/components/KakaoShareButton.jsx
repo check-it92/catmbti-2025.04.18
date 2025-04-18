9d4e23340dcf88baf9f3120e35f1acdabc3e6f12
check-it92 <worl5214@naver.com>
2025-04-18 12:23:43 +0900

    forth_commit

--- src/components/KakaoShareButton.jsx
+++ src/components/KakaoShareButton.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
 
 // 카카오는 윈도우 객체안에 SDK를 찾아오는 것이기 때문에 함수로 불러와야 한다.
-const { kakao } = window;
+const { Kakao } = window;
 
 const KakaoShareButton = () => {
   const url = "https://mbti-cats.netlify.app/"; // 공유하려는 페이지의 값
@@ -10,78 +10,33 @@ const KakaoShareButton = () => {
 
   console.log("TEST : ", url, resultURL);
 
-  useEffect =
-    (() => {
-      kakao.cleanup(); // 기존(앞단의 캐쉬값)입력 값이 있다면 정리해주는 함수
-      kakao.init("440a5a33bff6aef998285947dcbb1d5d"); // 초기값 입력함수
-    },
-    []); // 사용자가 처음 페이지 마운트를 하였을때의 목록을 가져와라
+  useEffect(() => {
+    Kakao.cleanup(); // 기존(앞단의 캐쉬값)입력 값이 있다면 정리해주는 함수
+    Kakao.init("440a5a33bff6aef998285947dcbb1d5d"); // 초기값 입력함수
+    Kakao.isInitialized();
+  }, []); // 사용자가 처음 페이지 마운트를 하였을때의 목록을 가져와라
 
   // 카카오 API를 활용하기 위한 변수 설정
   const sharekakao = () => {
     Kakao.Share.sendDefault({
       objectType: "feed",
       content: {
-        title: "오늘의 디저트",
-        description: "아메리카노, 빵, 케익",
+        title: "예비집사 판별기 결과",
+        description:
+          "예비집사님이 고양이를 키운다면 잘 맞는 고양이는 먼치킨입니다.",
         imageUrl:
           "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
         link: {
-          mobileWebUrl: "https://developers.kakao.com",
-          webUrl: "https://developers.kakao.com",
+          mobileWebUrl: resultURL,
+          webUrl: resultURL,
         },
-      },
-      itemContent: {
-        profileText: "Kakao",
-        profileImageUrl:
-          "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-        titleImageUrl:
-          "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-        titleImageText: "Cheese cake",
-        titleImageCategory: "Cake",
-        items: [
-          {
-            item: "Cake1",
-            itemOp: "1000원",
-          },
-          {
-            item: "Cake2",
-            itemOp: "2000원",
-          },
-          {
-            item: "Cake3",
-            itemOp: "3000원",
-          },
-          {
-            item: "Cake4",
-            itemOp: "4000원",
-          },
-          {
-            item: "Cake5",
-            itemOp: "5000원",
-          },
-        ],
-        sum: "총 결제금액",
-        sumOp: "15000원",
-      },
-      social: {
-        likeCount: 10,
-        commentCount: 20,
-        sharedCount: 30,
       },
       buttons: [
         {
-          title: "웹으로 이동",
+          title: "나도 테스트 하러가기",
           link: {
-            mobileWebUrl: "https://developers.kakao.com",
-            webUrl: "https://developers.kakao.com",
-          },
-        },
-        {
-          title: "앱으로 이동",
-          link: {
-            mobileWebUrl: "https://developers.kakao.com",
-            webUrl: "https://developers.kakao.com",
+            mobileWebUrl: url,
+            webUrl: url,
           },
         },
       ],
